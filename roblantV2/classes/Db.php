efec3918708c2fb6bfba2011a36254b409c92965
M.Sato <72994033+sakamotosensei@users.noreply.github.com>
2021-04-03 00:51:14 +0200

    Update Db.php

--- roblantV2/classes/Db.php
+++ roblantV2/classes/Db.php
@@ -5,7 +5,7 @@
 
         public static function getConnection(){
             if(self::$conn === null){
-                self::$conn = new PDO('mysql:host=localhost;dbname=hazuktr389_roblant', "hazuktr389_masa", "roblant2709");
+                self::$conn = new PDO('mysql:host=localhost;dbname=hazuktr389_roblant', "user", "password");
                 return self::$conn;
             }else{
                 return self::$conn;
